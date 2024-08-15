58634f3c5e1ed525aaa5a5a2c6bdfe1a9819c715
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-08-14 21:01:42 -0500

    Sincronización de tablas en base de datos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+const sequelize = require('./config/database');
+require('./models/student');
+require('./models/course');
+require('./models/enrollment');
+
 const app = require('./app'); // Importación del módulo app
 
 // Configuración del puerto
 const PORT = process.env.PORT || 3000;
+sequelize.sync()
+  .then(() => {
+    console.log('Tablas creadas correctamente.');
 
-// Iniciando el servidor en el puerto configurado
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+    // Iniciar el servidor después de la sincronización
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('No se pudo sincronizar la base de datos:', err);
 });
