dc16a334e1e01f364f9bae3b0b63f00e9dafe550
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-08-14 19:44:09 -0500

    Agrega app.js configuracion y sequelize dependencia

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const studentRoutes = require('./routes/studentRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+const enrollmentRoutes = require('./routes/enrollmentRoutes');
+
+const app = express();
+
+// Middleware para procesar JSON
+app.use(express.json());
+
+app.use('/students', studentRoutes);
+app.use('/courses', courseRoutes);
+app.use('/enrollments', enrollmentRoutes);
+
+// Exportación del módulo app para que pueda ser usado en index.js.
+module.export = app;
