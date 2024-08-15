57a31154606af1eb97580582bd60574c0a90236e
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-08-14 20:40:18 -0500

    Debuggeo de consulta SQL

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -13,6 +13,7 @@ const createStudent = async (req, res) => {
     try {
       const { first_name, last_name, email } = req.body;
       const student = await Student.create({ first_name, last_name, email });
+      console.log('Students:', students);
       res.status(201).json(student);
     } catch (err) {
       res.status(500).json({ error: 'Error al crear el estudiante.' });
