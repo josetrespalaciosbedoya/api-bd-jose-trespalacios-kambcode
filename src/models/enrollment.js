fe5e6eb7aec888226e11e52387090402408763eb
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-08-16 19:58:14 -0500

    Base estable de proyecto api

--- src/models/enrollment.js
+++ src/models/enrollment.js
@@ -4,10 +4,18 @@ const Student = require('./student');
 const Course = require('./course');
 
 const Enrollment = sequelize.define('Enrollment', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
   grade: {
     type: DataTypes.STRING,
     allowNull: true,
   },
+}, {
+  timestamps: true, // Esto crea automáticamente las columnas createdAt y updatedAt
+  tableName: 'enrollments', // Asegúrate de que el nombre de la tabla coincida con el de la BD
 });
 
 // Definir las relaciones
